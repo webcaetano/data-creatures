666c1e5322e8af3cfdbcb1df1d16828e227a3d4c
Andre Caetano <webandrecaetano@gmail.com>
2016-07-19 22:19:56 -0300

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+var path = require('path');
+var fs = require('fs');
+var _ = require('lodash');
+
+// insert defaults here
+var defaults = {
+
+}
+
+var self = function(options){
+	options = _.extend({},defaults,options);
+
+}
+
+module.exports = self;
---
+++ test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+var self = require('./');
+
+describe('data-creatures', function() {
+
+	it('should do something', function(done) {
+		done();
+	});
+})
