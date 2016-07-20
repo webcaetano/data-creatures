a18e0109b0dd32a17444d26c56a4a769c9c0bc6d
Andre Caetano <webandrecaetano@gmail.com>
2016-07-19 23:25:27 -0300

    write test

--- test.js
+++ test.js
@@ -1,13 +1,34 @@
 var expect = require('chai').expect;
+var _ = require('lodash');
 var self = require('./');
 
 describe('data-creatures', function() {
 
-	it('should do something', function(done) {
-		console.log(self({
-			size:2,
+	it('should return all  data', function(done) {
+		var resp = self();
+
+		expect(resp).length.to.be.gt(30);
+		done();
+	});
+
+	it('should return a sample with certain size', function(done) {
+		var resp = self({
+			size:10
+		});
+
+		expect(resp).length.to.be.lengthOf(10);
+		done();
+	});
+
+	it('should pluralize', function(done) {
+		var resp = self({
 			plural:true
-		}))
+		});
+
+		expect(_.includes(resp,'orcs')).to.be.true;
+		expect(_.includes(resp,'elves')).to.be.true;
+		expect(_.includes(resp,'dragons')).to.be.true;
+		expect(resp).length.to.be.gt(30);
 		done();
 	});
 })
